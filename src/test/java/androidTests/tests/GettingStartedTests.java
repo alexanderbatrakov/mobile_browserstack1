7fd41e9e2c525fe4edcc5065e8ffd5b585867237
Alexander Batrakov <a.batrakov1@gmail.com>
2023-02-12 14:09:09 +0400

    Added Wiki onboarding test

--- src/test/java/androidTests/tests/GettingStartedTests.java
+++ src/test/java/androidTests/tests/GettingStartedTests.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.id;
 
-@Tag("android")
 public class GettingStartedTests extends TestBase {
 
     @Test
